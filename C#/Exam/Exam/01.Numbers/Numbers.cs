918cac7c397f922fec889337c151477cf3181735
Spas Vutov <VutovSpas@gmail.com>
2014-11-29 11:13:47 +0200

    Add
    OOP exam on C#

---
+++ C#/Exam/Exam/01.Numbers/Numbers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Numbers
+{
+    static void Main(string[] args)
+    {
+        List<int> randomNumbers = new List<int>();
+        Random rng = new Random();
+        for (int i = 0; i < 15; i++)
+        {
+            randomNumbers.Add(rng.Next(-150, 250));
+        }
+        Console.WriteLine("Enter number: ");
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine("Numbers in the list:");
+        bool found = false;
+        foreach (int num in randomNumbers)
+        {
+            Console.Write(num + " ");
+            if (number == num)
+            {
+                found = true;
+            }
+        }
+        Console.WriteLine();
+        if (found)
+        {
+            Console.WriteLine("{0} is part of the list.", number);
+        }
+        else
+        {
+            Console.WriteLine("{0} is NOT part of the list.", number);
+        }
+
+    }
+}
---
+++ C#/Exam/Exam/02.Prism/Prism.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Prism
+{
+    private double a;
+    private double b;
+    private double h;
+
+    public Prism(double a, double b, double h)
+    {
+        this.a = a;
+        this.b = b;
+        this.h = h;
+    }
+
+    public void Show()
+    {
+        Console.WriteLine("Side one is {0}\nSide two is {1}\nh is {2}", a, b, h);
+    }
+
+    public void Volume()
+    {
+        double v = a * b * h;
+        Console.WriteLine("Volume of the prism is {0}", v);
+    }
+
+    public void Surface()
+    {
+        double v = 2 * (a * b + a * h + b * h);
+        Console.WriteLine("Surface of the prism is {0}", v);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter side a: ");
+        double a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter side b: ");
+        double b = double.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter side h: ");
+        double h = double.Parse(Console.ReadLine());
+        Prism myPrism = new Prism(a, b, h);
+
+        myPrism.Show();
+        myPrism.Volume();
+        myPrism.Surface();
+
+    }
+}
